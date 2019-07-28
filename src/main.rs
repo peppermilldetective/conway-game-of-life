38817d3733d42441de41d92f134764e3136ba0ba
Jarrett P. Locke <jarrett.p.locke@gmail.com>
2019-07-28 16:20:32 -0500

    Working code commit. It functions and works properly (from what is seen). Need to add tests and restructure to make the code more expandable.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate glium;
+extern crate rand;
 
 #[derive(Copy, Clone)]
 struct Vertex {
@@ -46,28 +47,88 @@ impl<'a> Shaders<'a> {
 
 #[derive(Copy, Clone)]
 struct Cell {
-   position: [f32; 2]
+   position: [i32; 2],
+   draw_coord: [f32; 2],
+   deltas: [f32; 2],
+   alive: u32
 }
 
 trait Drawable {
-   fn draw(&self);
+   fn draw(&self,
+      vertex_buffer: &glium::VertexBuffer<Vertex>,
+      index_buffer: &glium::IndexBuffer<u16>,
+      program: &glium::Program,
+      frame: &mut glium::Frame);
+   fn create_buffers(&self, display: &glium::Display) -> (glium::VertexBuffer<Vertex>, glium::IndexBuffer<u16>);
 }
 
 impl Cell {
-   fn new(x: f32, y: f32) -> Cell {
+   fn new(x: i32, y: i32, draw_x: f32, draw_y: f32, d_x: f32, d_y: f32, alive: u32) -> Cell {
       Cell {
-         position: [x, y]
+         position: [x, y],
+         draw_coord: [draw_x, draw_y],
+         deltas: [d_x, d_y],
+         alive
       }
    }
 }
 
 impl Drawable for Cell {
-   fn draw(&self) {
-      // TODO
+   fn draw(&self,
+      vertex_buffer: &glium::VertexBuffer<Vertex>,
+      index_buffer: &glium::IndexBuffer<u16>,
+      program: &glium::Program,
+      frame: &mut glium::Frame)
+   {
+   use glium::Surface;
+
+   if self.alive == 0 {
+      return;
+   }
+
+   let uniforms = uniform! {
+   };
+
+   frame
+      .draw(
+         vertex_buffer,
+         index_buffer,
+         program,
+         &uniforms,
+         &Default::default(),
+      )
+      .unwrap();
+   }
+
+   fn create_buffers(&self, display: &glium::Display) -> (glium::VertexBuffer<Vertex>, glium::IndexBuffer<u16>) {
+      use glium::{
+         VertexBuffer,
+         IndexBuffer,
+         index::PrimitiveType
+      };
+
+      let x = self.draw_coord[0];
+      let y = self.draw_coord[1];
+      let d_x = self.deltas[0];
+      let d_y = self.deltas[1];
+
+      let shape = vec![
+         Vertex { position: [ x      , y      ] },
+         Vertex { position: [ x + d_x, y      ] },
+         Vertex { position: [ x      , y + d_y] },
+         Vertex { position: [ x + d_x, y + d_y] },
+      ];
+
+      (
+         VertexBuffer::new(display, &shape).unwrap(),
+         IndexBuffer::new(display, PrimitiveType::TrianglesList, &[0u16, 1, 2, 1, 2, 3]).unwrap()
+      )
    }
 }
 
 struct Grid {
+   width: i32,
+   height: i32,
    cells: Vec<Cell>
 }
 
@@ -75,98 +136,159 @@ impl Grid {
    fn new(width: i32, height: i32) -> Grid {
       let mut cells: Vec<Cell> = Vec::new();
 
-      for x in 0..width {
-         for y in 0..height {
+      let d_x = 2.0 / width as f32;
+      let d_y = 2.0 / height as f32;
+
+      for y in 0..height {
+         for x in 0..width {
+            let draw_x = -1.0 + (d_x * x as f32);
+            let draw_y = -1.0 + (d_y * y as f32);
+
             cells.push(Cell::new(
-               x as f32,
-               y as f32
+               x,
+               y,
+               draw_x,
+               draw_y,
+               d_x,
+               d_y,
+               rand::random::<u32>() % 2
             ));
          }
       }
 
       Grid {
+         width,
+         height,
          cells
       }
    }
 
-   fn draw_grid(grid: Grid) {
-      for cell in grid.cells {
-         cell.draw();
+   fn draw_grid(grid: &Grid, display: &glium::Display, program: &glium::Program) {
+      use glium::Surface;
+
+      let mut frame = display.draw();
+      frame.clear_color(0.0, 0.0, 0.0, 1.0);
+
+      let cells = &grid.cells;
+
+      for cell in cells {
+         let (vert_buf, ind_buf) = cell.create_buffers(display);
+         cell.draw(&vert_buf, &ind_buf, program, &mut frame);
       }
+
+      frame.finish().unwrap();
+   }
+
+   fn next_state(grid: &mut Grid) {
+      let mut next_state: Vec<Cell> = Vec::new();
+
+      for (i, cell) in grid.cells.iter().enumerate() {
+         let i = i as i32;
+         let width = grid.width;
+         let area = width * grid.height;
+
+         let up = ((i + area) + width)%area;
+         let down = ((i + area) - width)%area;
+         let left = (width * (i/width)) + ((i + width) - 1)%width;
+         let right = (width * (i/width)) + ((i + width) + 1)%width;
+         let ul = (width * (up/width)) + ((up + width) - 1)%width;
+         let ur = (width * (up/width)) + ((up + width) + 1)%width;
+         let ll = (width * (down/width)) + ((down + width) - 1)%width;
+         let lr = (width * (down/width)) + ((down + width) + 1)%width;
+
+         let sum =
+            grid.cells[ul as usize].alive + 
+            grid.cells[up as usize].alive +
+            grid.cells[ur as usize].alive + 
+            grid.cells[left as usize].alive +
+            grid.cells[right as usize].alive +
+            grid.cells[ll as usize].alive +
+            grid.cells[down as usize].alive +
+            grid.cells[lr as usize].alive;
+
+         let alive = 
+            if cell.alive == 1 && (sum < 2 || sum > 3) {
+               0
+            }
+            else if cell.alive == 0 && sum == 3 {
+               1
+            }
+            else {
+               cell.alive
+            };
+
+         next_state.push(Cell::new(
+            cell.position[0],
+            cell.position[1],
+            cell.draw_coord[0],
+            cell.draw_coord[1],
+            cell.deltas[0],
+            cell.deltas[1],
+            alive
+         ));
+      }
+
+      grid.cells = next_state;
    }
 }
 
-fn create_buffers(display: &glium::Display) -> (glium::VertexBuffer<Vertex>, glium::IndexBuffer<u16>) {
-   use glium::{
-      VertexBuffer,
-      IndexBuffer,
-      index::PrimitiveType
-   };
-
-   let shape = vec![
-      Vertex { position: [-0.5,  0.5] },
-      Vertex { position: [-0.5, -0.5] },
-      Vertex { position: [ 0.5, -0.5] },
-      Vertex { position: [ 0.5,  0.5] },
-   ];
-
-   (
-      VertexBuffer::new(display, &shape).unwrap(),
-      IndexBuffer::new(display, PrimitiveType::TrianglesList, &[0u16, 1, 2, 0, 2, 3]).unwrap()
-   )
-}
-
 fn main() {
-   use glium::{glutin, Surface};
-
-   let width = 1024.0f64;
-   let height =1024.0f64;
+   let width = 720.0f64;
+   let height = 720.0f64;
    let title = "Conway's Game of Life";
-   
-   let rows = 50;
-   let columns = 50;
-   let cell_count = rows * columns;
 
-   let grid = Grid::new(rows, columns);
-
-   let mut events_loop = glutin::EventsLoop::new();
-   let wb = glutin::WindowBuilder::new()
-      .with_dimensions(glutin::dpi::LogicalSize::new(width as f64, height as f64))
-      .with_title(title);
-   let cb = glutin::ContextBuilder::new();
-   let display = glium::Display::new(wb, cb, &events_loop).unwrap();
+   let (mut events_loop, display) = create_display(width as f64, height as f64, title);
 
    let shaders = Shaders::new();
 
    let program =
       glium::Program::from_source(&display, shaders.vert, shaders.frag, None).unwrap();
 
-   let (vertex_buffer, index_buffer) = create_buffers(&display);
+   main_loop(&mut events_loop, &program, &display);
+}
+
+fn main_loop(
+   events_loop: &mut glium::glutin::EventsLoop,
+   program: &glium::Program,
+   display: &glium::Display)
+{
+   use glium::glutin::{ Event, WindowEvent };
+
+   let rows = 50;
+   let columns = 50;
+
+   let mut grid = Grid::new(rows, columns);
 
    let mut closed = false;
    while !closed {
-      let uniforms = uniform! {
-      };
-
-      let mut frame = display.draw();
-      frame.clear_color(0.0, 0.0, 0.0, 1.0);
-      frame
-         .draw(
-            &vertex_buffer,
-            &index_buffer,
-            &program,
-            &uniforms,
-            &Default::default(),
-         )
-         .unwrap();
-      frame.finish().unwrap();
+      Grid::draw_grid(&grid, &display, program);
 
       events_loop.poll_events(|ev| match ev {
-         glutin::Event::WindowEvent { event, .. } => match event {
-            glutin::WindowEvent::CloseRequested => closed = true,
+         Event::WindowEvent { event, .. } => match event {
+            WindowEvent::CloseRequested => closed = true,
             _ => (),
          },
          _ => (),
       });
+
+      Grid::next_state(&mut grid);
    }
+}
+
+fn create_display(width: f64, height: f64, title: &str) -> (glium::glutin::EventsLoop, glium::Display) {
+   use glium::glutin::{
+      EventsLoop,
+      dpi,
+      ContextBuilder,
+      WindowBuilder
+   };
+
+   let events_loop = EventsLoop::new();
+   let wb = WindowBuilder::new()
+      .with_dimensions(dpi::LogicalSize::new(width as f64, height as f64))
+      .with_title(title);
+   let cb = ContextBuilder::new();
+   let display = glium::Display::new(wb, cb, &events_loop).unwrap();
+
+   (events_loop, display)
 }
